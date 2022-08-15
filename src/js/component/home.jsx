532ef4f4e583b917da48a614a8dae879ae693bad
joce2311 <jnortegamontecino@gmail.com>
2022-08-15 04:20:46 +0000

    terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,58 @@
-import React from "react";
+import React, { useState} from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export const TodoList = () => {
+	const [list, setlist] = useState([]);
+	const [valor, setvalor] = useState();
 
-//create your first component
-const Home = () => {
+	const addList = e => {
+		if (e.key === "Enter" && e.target.value !== "") {
+			
+			for(let i = 0; i < e.target.value.length ; i++){
+				if(e.target.value[i] != " "){
+					setlist([...list, e.target.value]);
+					setvalor("");
+					break
+				
+
+				}
+			}
+		}
+	};
+	const DeleteItems = (indexItem) => {
+		setlist((prevState) =>
+			prevState.filter((todo, index) => index !== indexItem)
+		);
+	};
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+		<div className="container text-center">
+			<h1 className="container d-flex justify-content-center text-center col-5 bi bi-x-lg">TODOOlist.. (tarea)</h1>
+			<div className="container shadow p-1 bg-white rounded col-5 bi bi-x-lg">
+				<input
+					onChange={(e)=>{ setvalor(e.target.value)}}
+					type="text"
+					placeholder="Dolor de cabeza :D"
+					className="col-8 border none text-center"
+					onKeyPress={addList}
+					value = {valor}
+				/>
+			</div>
 
-export default Home;
+			<div className="text-center">
+				<ul class="list-group">
+					{list.map((item, index) => (
+						<li 
+							class="container list-group-item p-1 col-5 bi bi-x-lg d-flex justify-content-between"
+							key={index}>
+								{item}
+							<button className="btn-close" onClick={() => DeleteItems(index)}>
+						
+							</button>
+						</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	)
+}
+
+export default TodoList;
